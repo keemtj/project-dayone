a93697394f95ea806df0cc8ded3a6d07ac643be9
JimmyJoo <jimmyjoo1029@gmail.com>
2020-07-03 15:57:33 +0900

    :pencil: Add servers
    - Add server directory \n - Add a testing api that makes a request to the server

---
+++ src/Api/userApi.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import axios from 'axios';
+
+const client = axios.create();
+
+export const userApi = {
+  getUsers: () => client.get('/api/users').then((r) => r.data),
+};
+
+export default userApi;
--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import userApi from './Api/userApi';
 
-function App() {
-  return <div>TEST</div>;
-}
+const App = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const getUsers = async () => {
+      const users = await userApi.getUsers();
+      setUsers(users);
+    };
+    getUsers();
+    // fetch('api')
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log('data: ', data);
+    //     setUser(data);
+    //   });
+    console.log('useEffect ends...');
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {users.map((user) => (
+          <li key={user.id}>
+            <span>{user.id}</span>
+            <span>{user.userId}</span>
+            <span>{user.userPw}</span>
+          </li>
+        ))}
+      </header>
+    </div>
+  );
+};
 
 export default App;
