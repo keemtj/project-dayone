5c8244df2a4114a78da86c46d7fbc5d266df0746
Leehaeun0 <a01050675111@gmail.com>
2020-07-03 22:43:48 +0900

    :pencil: add form for Diary.js

--- src/Page/Diary.js
+++ src/Page/Diary.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Style/Diary.module.scss';
+
+const cx = classNames.bind(styles);
 
 const Diary = () => {
+  const onClick = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>다이어리</h1>
-      <p>다이어리, 일기는 여기서 쓰면 된단다</p>
+    <div className={cx('wrapper')}>
+      <h1 className={cx('a11yHidden')}>다이어리</h1>
+      <form action="#">
+        <input type="text" className={cx('title')} />
+        <textarea
+          name="diaryContent"
+          id="diaryContent"
+          className={cx('diaryContent')}
+          cols="30"
+          rows="10"
+        />
+        <button type="submit" onClick={onClick}>
+          완료
+        </button>
+      </form>
     </div>
   );
 };
