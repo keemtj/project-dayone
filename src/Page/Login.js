127dbf6ba4da7e27d6892def66cf0fdfcb1ea632
keemtj <keemgreat@gmail.com>
2020-07-04 20:13:48 +0900

    :lipstick: Add login form UI

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
 import styles from './Style/Login.module.scss';
 
 const cx = classNames.bind(styles);
@@ -14,9 +14,11 @@ const Login = () => {
       <form>
         <input type="text" placeholder="아이디" />
         <input type="password" placeholder="비밀번호" />
-        {accountCheck || (
-          <span>가입하지 않은 아이디이거나, 잘못된 비밀번호입니다.</span>
-        )}
+        <span className={cx('message')}>
+          {accountCheck
+            ? '가입하지 않은 아이디이거나, 잘못된 비밀번호 입니다.'
+            : ''}
+        </span>
         <button type="submit">로그인</button>
       </form>
     </div>
