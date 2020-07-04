4bc3aec3fd5e2c0cb23bba91e6c7d67ae8174825
keemtj <keemgreat@gmail.com>
2020-07-04 17:07:00 +0900

    :sparkles: Change login component structure

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import classNames from 'classnames';
+import styles from './Style/Login.module.scss';
+
+const cx = classNames.bind(styles);
 
 const Login = () => {
+  // 임시로 설정
+  const accountCheck = false;
+
   return (
-    <div>
-      <h1>로그인</h1>
-      <p>로그인, 여기서 로그인하고 일기써라</p>
+    <div className={cx('loginWrapper')}>
+      <h1>DAY ONE</h1>
+      <form>
+        <input type="text" placeholder="아이디" />
+        <input type="password" placeholder="비밀번호" />
+        {accountCheck || (
+          <span>가입하지 않은 아이디이거나, 잘못된 비밀번호입니다.</span>
+        )}
+        <button type="submit">로그인</button>
+      </form>
     </div>
   );
 };
