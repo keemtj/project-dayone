a2643637824fa3e8215348d51b4a10f19f55bf4b
kimfriendship <kfriendship94@gmail.com>
2020-07-05 15:58:19 +0900

    :construction: Working on server

--- src/Api/userApi.js
+++ src/Api/userApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const client = axios.create();
 
 export const userApi = {
-  getUsers: () => client.get('/api/users').then((r) => r.data),
+  getUsers: () => client.get('/api/users').then((r) => console.log(r.data)),
 };
 
 export default userApi;
