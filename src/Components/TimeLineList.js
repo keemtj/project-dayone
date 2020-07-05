15840c2827488ed8832fbe593ac75fd7fc53fce4
keemtj <keemgreat@gmail.com>
2020-07-05 15:15:09 +0900

    :construction: Organize TimeLine structure

---
+++ src/Components/TimeLineList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TimeLineList = () => {
+  return (
+    <li>
+      <figure>
+        <img src="#" alt="thumbnail" />
+        <figcaption>리스트 제목</figcaption>
+      </figure>
+    </li>
+  );
+};
+
+export default TimeLineList;
--- src/Page/TimeLine.js
+++ src/Page/TimeLine.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import {
+  faThLarge,
+  faThList,
+  faSquare,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './Style/TimeLine.module.scss';
+import TimeLineList from '../Components/TimeLineList';
+
+const cx = classNames.bind(styles);
 
 const Timeline = () => {
   return (
     <div>
-      <h1>타임라인</h1>
-      <p>타임라인, 여기에 네가 쓴 일기가 다 보인단다</p>
+      <FontAwesomeIcon icon={faThLarge} className={cx('icon')} />
+      <FontAwesomeIcon icon={faThList} className={cx('icon')} />
+      <FontAwesomeIcon icon={faSquare} className={cx('icon')} />
+      <div>
+        <ul className={cx('timelineList')}>
+          <TimeLineList />
+          <TimeLineList />
+          <TimeLineList />
+          <TimeLineList />
+          <TimeLineList />
+          <TimeLineList />
+          <TimeLineList />
+          <TimeLineList />
+          <TimeLineList />
+          <TimeLineList />
+          <TimeLineList />
+          <TimeLineList />
+          <TimeLineList />
+          <TimeLineList />
+          <TimeLineList />
+        </ul>
+      </div>
     </div>
   );
 };
