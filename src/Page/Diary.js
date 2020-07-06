bb8f88d2157b92fd56ee4958d5a24e150dec1d3e
Leehaeun0 <a01050675111@gmail.com>
2020-07-06 14:17:29 +0900

    :lipstick: add midea query for css

--- src/Page/Diary.js
+++ src/Page/Diary.js
@@ -12,19 +12,6 @@ const Diary = () => {
   return (
     <div className={cx('wrapper')}>
       <h1 className={cx('a11yHidden')}>다이어리</h1>
-      <form action="#">
-        <input type="text" className={cx('title')} />
-        <textarea
-          name="diaryContent"
-          id="diaryContent"
-          className={cx('diaryContent')}
-          cols="30"
-          rows="10"
-        />
-        <button type="submit" onClick={onClick}>
-          완료
-        </button>
-      </form>
     </div>
   );
 };
