b64877c07307f82baf6cf4cd83139c9415f32d55
Leehaeun0 <a01050675111@gmail.com>
2020-07-06 14:25:39 +0900

    :construction: remove event

--- src/Page/Diary.js
+++ src/Page/Diary.js
@@ -5,10 +5,6 @@ import styles from './Style/Diary.module.scss';
 const cx = classNames.bind(styles);
 
 const Diary = () => {
-  const onClick = (e) => {
-    e.preventDefault();
-  };
-
   return (
     <div className={cx('wrapper')}>
       <h1 className={cx('a11yHidden')}>다이어리</h1>
