926a664f51ae38cf379a188d3d9b85222164a69e
kimfriendship <kfriendship94@gmail.com>
2020-07-06 14:39:34 +0900

    :construction: Add calendar reducer

--- src/Reducer/calendarReducer.js
+++ src/Reducer/calendarReducer.js
@@ -1,3 +1,30 @@
-export const calendarInitState = {};
+export const calendarInitState = {
+  loading: false,
+  error: null,
+  diaries: [],
+};
 
-export const calendarReducer = (state, action) => {};
+export const calendarReducer = (state, action) => {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        loading: true,
+        error: null,
+        diaries: state.diaries,
+      };
+    case 'ERROR':
+      return {
+        loading: false,
+        error: action.error,
+        diaries: null,
+      };
+    case 'SUCCESS':
+      return {
+        loading: false,
+        error: null,
+        diaries: action.diaries,
+      };
+    default:
+      throw new Error(`${action.type}: ERROR`);
+  }
+};
