38702bf14c476bd2a5c039dcef3ef5bb9c44cff0
keemtj <keemgreat@gmail.com>
2020-07-10 23:55:59 +0900

    :construction: Add Route, Link to DiaryViewer

--- src/Components/TimeLineList.js
+++ src/Components/TimeLineList.js
@@ -1,27 +1,32 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
+import DiaryViewer from '../Page/DiaryViewer';
 import styles from './Style/TimeLineList.module.scss';
 
 const cx = classNames.bind(styles);
 
 const TimeLineList = ({ diary }) => {
-  const { title, content, date, imagePaths } = diary;
+  const { id, title, content, date, imagePaths } = diary;
   console.log(content, date);
 
   return (
-    <li className={cx('timelineList')}>
-      <figure>
-        <img
-          src={
-            imagePaths.length !== 0
-              ? imagePaths[0]
-              : 'https://thumbs.dreamstime.com/b/no-image-available-icon-photo-camera-flat-vector-illustration-132483141.jpg'
-          }
-          alt=""
-        />
-        <figcaption>{title}</figcaption>
-      </figure>
-    </li>
+    <Link to={`/diaryViewer/${id}`}>
+      <li className={cx('timelineList')}>
+        <figure>
+          <img
+            src={
+              imagePaths.length !== 0
+                ? imagePaths[0]
+                : 'https://thumbs.dreamstime.com/b/no-image-available-icon-photo-camera-flat-vector-illustration-132483141.jpg'
+            }
+            alt=""
+          />
+          <figcaption>{title}</figcaption>
+        </figure>
+      </li>
+      <Route path="/diaryViewer/:id" component={DiaryViewer} />
+    </Link>
   );
 };
 
