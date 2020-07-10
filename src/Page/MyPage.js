d021850ba9c859877375320f52ee4430fcf2adff
kimfriendship <kfriendship94@gmail.com>
2020-07-10 21:15:03 +0900

    :sparkles: Add mark up of my page

--- src/Page/MyPage.js
+++ src/Page/MyPage.js
@@ -12,22 +12,30 @@ const MyPage = () => {
 
   return (
     <main>
-      <h2 className={cx('greeting')}>안녕하세요 {userId}님!</h2>
-      <ul>
-        <li>
-          <span>235</span>
-          <span>일기수</span>
-        </li>
-        <li>
-          <span>23</span>
-          <span>북마크수</span>
-        </li>
-        <li>
-          <span>2.3</span>
-          <span>평균 일기수</span>
-        </li>
+      <aside>
+        <h2 className={cx('greeting')}>안녕하세요 {userId}님!</h2>
+        <ul className={cx('statistics')}>
+          <li>
+            <span className={cx('numbers')}>235</span>
+            <span>일기수</span>
+          </li>
+          <li>
+            <span className={cx('numbers')}>23</span>
+            <span>북마크수</span>
+          </li>
+          <li>
+            <span className={cx('numbers')}>2.3</span>
+            <span>평균 일기수</span>
+          </li>
+        </ul>
+        <button type="button">로그아웃</button>
+      </aside>
+      <h3 className={cx('listTitle')}>북마크 다이어리 리스트</h3>
+      <ul className={cx('diaryList')}>
+        <li>다이어리1</li>
+        <li>다이어리2</li>
+        <li>다이어리3</li>
       </ul>
-      <p>마이 페이지, 네 자신을 알라</p>
     </main>
   );
 };
