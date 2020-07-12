739d737f54a79c03bd64c7749555a1c776b66411
Leehaeun0 <a01050675111@gmail.com>
2020-07-12 23:44:15 +0900

    :construction: work in progerss

--- src/Page/DiaryViewer.js
+++ src/Page/DiaryViewer.js
@@ -4,21 +4,17 @@ import { useParams } from 'react-router-dom';
 import { MainContext } from '../Context/MainContext';
 
 const DiaryViewer = () => {
-  const { state, getDiary, clearViewerDiary } = React.useContext(MainContext);
+  const { state, getDiary } = React.useContext(MainContext);
   const { id } = useParams();
 
   useEffect(() => {
     getDiary(id);
-    return () => {
-      // state.viewerDiary = {};
-      clearViewerDiary();
-    };
     // console.log('viewer', state.viewerDiary);
   }, []);
 
   // const diary = state.diaries.find((v) => v.id === +id);
   const renderDiary = () => {
-    if (!state.viewerDiary.body) return '';
+    if (!state.viewerDiary.body) return;
     return <div dangerouslySetInnerHTML={{ __html: state.viewerDiary.body }} />;
   };
 
@@ -26,8 +22,7 @@ const DiaryViewer = () => {
     <div>
       {console.log('viewer', state.viewerDiary)}
       <h1>다이어리 뷰어</h1>
-      {console.log(state.viewerDiary)}
-      {/* {renderDiary()} */}
+      {renderDiary()}
       {/* <div dangerouslySetInnerHTML={{ __html: state.viewerDiary.body }} /> */}
     </div>
   );
