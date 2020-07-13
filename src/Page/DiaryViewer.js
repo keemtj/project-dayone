deaad1a9d6b80855255a4ea166fe2baf65b8cee6
Leehaeun0 <a01050675111@gmail.com>
2020-07-13 14:02:43 +0900

    :construction: work in progress

--- src/Page/DiaryViewer.js
+++ src/Page/DiaryViewer.js
@@ -4,17 +4,21 @@ import { useParams } from 'react-router-dom';
 import { MainContext } from '../Context/MainContext';
 
 const DiaryViewer = () => {
-  const { state, getDiary } = React.useContext(MainContext);
+  const { state, getDiary, clearViewerDiary } = React.useContext(MainContext);
   const { id } = useParams();
 
   useEffect(() => {
     getDiary(id);
+    return () => {
+      // state.viewerDiary = {};
+      clearViewerDiary();
+    };
     // console.log('viewer', state.viewerDiary);
   }, []);
 
   // const diary = state.diaries.find((v) => v.id === +id);
   const renderDiary = () => {
-    if (!state.viewerDiary.body) return;
+    if (!state.viewerDiary.body) return '';
     return <div dangerouslySetInnerHTML={{ __html: state.viewerDiary.body }} />;
   };
 
@@ -22,7 +26,8 @@ const DiaryViewer = () => {
     <div>
       {console.log('viewer', state.viewerDiary)}
       <h1>다이어리 뷰어</h1>
-      {renderDiary()}
+      {console.log(state.viewerDiary)}
+      {/* {renderDiary()} */}
       {/* <div dangerouslySetInnerHTML={{ __html: state.viewerDiary.body }} /> */}
     </div>
   );
