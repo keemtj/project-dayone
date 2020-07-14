847e44195cf727f602716f2ef6f85efa1f5845d7
keemtj <keemgreat@gmail.com>
2020-07-15 01:24:10 +0900

    :lipstick: Add hover to TimeLineMedia

--- src/Components/TimeLineMedia.js
+++ src/Components/TimeLineMedia.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBookmark } from '@fortawesome/free-solid-svg-icons';
-import styles from './Style/TimeLineList.module.scss';
+import styles from './Style/TimeLineMedia.module.scss';
 import { MainContext } from '../Context/MainContext';
 
 const cx = classNames.bind(styles);
@@ -22,7 +22,7 @@ const TimeLineMedia = () => {
   return (
     <ul className={cx('timelineWrapper')}>
       {diaries.map((diary) => (
-        <li key={diary.id} className={cx('timeline-media')}>
+        <li key={diary.id} className={cx('timelineMedia')}>
           <Link to={`/diaryViewer/${diary.id}`}>
             <img
               className={cx('thumbnail')}
@@ -31,24 +31,35 @@ const TimeLineMedia = () => {
                   ? diary.imagePaths[0]
                   : 'https://user-images.githubusercontent.com/67693474/86562086-0998c900-bf9d-11ea-8a2b-66b4994e2072.png'
               }
-              alt="https://user-images.githubusercontent.com/67693474/86562086-0998c900-bf9d-11ea-8a2b-66b4994e2072.png"
+              alt="다이어리 첫번째 사진"
             />
           </Link>
-          {/* 마우스 hover시 보여지는 bookmark */}
-          <input
-            id={diary.id}
-            type="checkbox"
-            checked={diary.isBookmarked ? 'checked' : ''}
-            onChange={onChangeBookmark}
-          />
-          <label htmlFor={diary.id}>
-            <span>
-              <FontAwesomeIcon
-                icon={faBookmark}
-                className={cx('bookmarkIcon')}
-              />
-            </span>
-          </label>
+          {/* 마우스 hover시 보여지는 부분 */}
+          <div className={cx('overwrap')}>
+            <input
+              id={diary.id}
+              type="checkbox"
+              checked={diary.isBookmarked ? 'checked' : ''}
+              onChange={onChangeBookmark}
+            />
+            <label htmlFor={diary.id}>
+              <span>
+                <FontAwesomeIcon
+                  icon={faBookmark}
+                  className={cx('bookmarkIcon')}
+                />
+              </span>
+            </label>
+            <Link
+              to={`/diaryViewer/${diary.id}`}
+              style={{ textDecoration: 'none' }}
+            >
+              <div className={cx('diaryInfo')}>
+                <div className={cx('title')}>{diary.title}</div>
+                <div className={cx('date')}>{diary.date}</div>
+              </div>
+            </Link>
+          </div>
         </li>
       ))}
     </ul>
