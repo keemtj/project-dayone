a59e95364c1f64a5867cb9edac68e304468e0c12
keemtj <keemgreat@gmail.com>
2020-07-15 04:02:28 +0900

    :lipstick: Update NavLink text style

--- src/Page/TimeLine.js
+++ src/Page/TimeLine.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThLarge, faThList } from '@fortawesome/free-solid-svg-icons';
@@ -9,39 +9,17 @@ import SubRouter from '../Router/SubRouter';
 const cx = classNames.bind(styles);
 
 const Timeline = () => {
-  const [timelineNav, setTimeLineNav] = useState('list'); // list, media
-
-  const onClickNav = ({ target }) => {
-    if (target.classList.length === 2) return;
-    setTimeLineNav(target.className);
-  };
-
   return (
     <main>
       <div className={cx('timelineNav')}>
-        <Link to="/timeline/list">
-          <button
-            type="button"
-            onClick={onClickNav}
-            className={cx('list', `${timelineNav === 'list' ? 'active' : ''}`)}
-          >
-            <FontAwesomeIcon icon={faThList} className={cx('icon')} />
-            <span className={cx('tooltip')}>리스트</span>
-          </button>
-        </Link>
-        <Link to="/timeline/media">
-          <button
-            type="button"
-            onClick={onClickNav}
-            className={cx(
-              'media',
-              `${timelineNav === 'media' ? 'active' : ''}`,
-            )}
-          >
-            <FontAwesomeIcon icon={faThLarge} className={cx('icon')} />
-            <span className={cx('tooltip')}>사진</span>
-          </button>
-        </Link>
+        <NavLink to="/timeline/list" activeClassName={cx('active')}>
+          <FontAwesomeIcon icon={faThList} className={cx('icon')} />
+          <span className={cx('tooltip')}>리스트</span>
+        </NavLink>
+        <NavLink to="/timeline/media" activeClassName={cx('active')}>
+          <FontAwesomeIcon icon={faThLarge} className={cx('icon')} />
+          <span className={cx('tooltip')}>사진</span>
+        </NavLink>
         <SubRouter />
       </div>
     </main>
