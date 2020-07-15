ee4801e2d38dec2216e6a4c1c6c2a8c303d669f6
kimfriendship <kfriendship94@gmail.com>
2020-07-15 21:45:47 +0900

    :sparkles: Add profile picture and message feature

--- src/Components/ProfileInfo.js
+++ src/Components/ProfileInfo.js
@@ -11,27 +11,31 @@ const ProfileInfo = ({ pageCtx }) => {
 
   return (
     <div className={cx('profile')}>
-      <h2 className={cx('greeting')}>
-        안녕하세요
-        <span>
-          {userId}
-          님!
-        </span>
-      </h2>
-      <ul className={cx('statistics')}>
-        <li>
-          <span className={cx('numbers')}>{diaries.length}</span>
-          <span className={cx('caption')}>일기수</span>
-        </li>
-        <li>
-          <span className={cx('numbers')}>{bookmarked.length}</span>
-          <span className={cx('caption')}>북마크수</span>
-        </li>
-        <li>
-          <span className={cx('numbers')}>{diaryPerDay}</span>
-          <span className={cx('caption')}>평균 일기수</span>
-        </li>
-      </ul>
+      <img className={cx('profilePic')} src="" alt={userId} />
+      <div className={cx('infoWrapper')}>
+        <h2 className={cx('greeting')}>
+          안녕하세요
+          <span>
+            {userId}
+            님!
+          </span>
+        </h2>
+        <h3 className={cx('profileMsg')}>상태메시지 룰루랄라</h3>
+        <ul className={cx('statistics')}>
+          <li>
+            <span className={cx('numbers')}>{diaries.length}</span>
+            <span className={cx('caption')}>일기수</span>
+          </li>
+          <li>
+            <span className={cx('numbers')}>{bookmarked.length}</span>
+            <span className={cx('caption')}>북마크수</span>
+          </li>
+          <li>
+            <span className={cx('numbers')}>{diaryPerDay}</span>
+            <span className={cx('caption')}>평균 일기수</span>
+          </li>
+        </ul>
+      </div>
       <button type="button" className={cx('logoutBtn')} onClick={onClickLogOut}>
         <FontAwesomeIcon icon={faSignOutAlt} className={cx('icon')} />
         로그아웃
