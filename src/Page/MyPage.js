c8352ebbf939f1e70f47428aa17b7663ac5d7764
JimmyJoo <jimmyjoo1029@gmail.com>
2020-07-16 21:34:29 +0900

    :bug: Fix a method that calculates diaryPerDay

--- src/Page/MyPage.js
+++ src/Page/MyPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Style/MyPage.module.scss';
 import { MainContext, LoginContext } from '../Context/MainContext';
@@ -10,19 +10,21 @@ const cx = classNames.bind(styles);
 
 const MyPage = () => {
   const [modalState, setModalState] = useState(false);
-  const mainCtx = React.useContext(MainContext);
-  const loginCtx = React.useContext(LoginContext);
+  const mainCtx = useContext(MainContext);
+  const loginCtx = useContext(LoginContext);
   const { logOut } = loginCtx;
   const { dispatch, patchBookmark, editProfile } = mainCtx;
   const { diaries, userData } = mainCtx.state;
   const { userId, pic, msg } = userData;
   const bookmarked = diaries.filter(({ isBookmarked }) => isBookmarked);
 
-  const yy = diaries[0].date.split('-')[0];
-  const mm = diaries[0].date.split('-')[1];
-  const dd = diaries[0].date.split('-')[2];
+  const yy = diaries[diaries.length - 1].date.split('-')[0];
+  const mm = diaries[diaries.length - 1].date.split('-')[1] - 1;
+  const dd = diaries[diaries.length - 1].date.split('-')[2];
+
   const diaryTerm = Math.floor((Date.now() - Date.UTC(yy, mm, dd)) / 86400000);
-  const diaryPerDay = Number((diaries.length / diaryTerm).toFixed(1));
+
+  const diaryPerDay = Number((diaries.length / diaryTerm).toFixed(2));
 
   const onClickLogOut = () => {
     dispatch({ type: 'LOG_OUT' });
