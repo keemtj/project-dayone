09f3a4d71f78a4e448e664ece0520f57b428c83f
Leehaeun0 <a01050675111@gmail.com>
2020-07-17 03:15:05 +0900

    :sparkles: Add tag list for diary page

--- src/Components/Diary/DiaryTagBox.js
+++ src/Components/Diary/DiaryTagBox.js
@@ -1,15 +1,37 @@
-import React from 'react';
+/* eslint-disable react/no-array-index-key */
+import React, { useContext } from 'react';
 import classNames from 'classnames/bind';
 import styles from '../Style/DiaryTagBox.module.scss';
+import { MainContext } from '../../Context/MainContext';
 
 const cx = classNames.bind(styles);
 
 const DiaryTagBox = () => {
+  const { state } = useContext(MainContext);
+  const { tags } = state.currentDiary;
+
+  console.log(tags);
+
+  const renderTags = () => {
+    if (!tags.length)
+      return (
+        <>
+          <li className={cx('tag')}>#태그1</li>
+          <li className={cx('tag')}>#태그2</li>
+        </>
+      );
+    return tags.map((tag, i) => (
+      <li className={cx('tag')} key={i}>
+        {tag}
+      </li>
+    ));
+  };
+
   return (
     <div className={cx('tagBox')}>
       <input type="text" placeholder="태그를 입력하세요" />
       <button type="button">추가</button>
-      <ul className={cx('tagList')}></ul>
+      <ul className={cx('tagList')}>{renderTags()}</ul>
     </div>
   );
 };
